ea2bb9bab3f3fb78d7726fdf7dd4be29f35ec894
David Sánchez <davidslt+git@pm.me>
2023-05-14 00:27:52 +0100

    feat: random ID

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@
 module Main (main) where
 
 import Control.Arrow ((>>>))
+import Control.Monad.IO.Class (MonadIO)
 import Data.ByteString qualified as B
 import Data.ByteString.Base16 qualified as B16
 import Data.ByteString.Builder (Builder, lazyByteString, toLazyByteString, word16BE, word8)
@@ -19,21 +20,17 @@ import Network.Socket
     socket,
   )
 import Network.Socket.ByteString (recv, sendAll)
+import System.Random (genWord16, initStdGen)
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
   adrr : _ <- getAddrInfo Nothing (Just "8.8.8.8") (Just "53")
   sock <- socket (addrFamily adrr) Datagram defaultProtocol
   connect sock (addrAddress adrr)
-  sendAll sock $ BS.toStrict . toLazyByteString $ buildQuery "www.example.com" typeA
-  -- _checkDNSQuery sock
+  _ <- sendAll sock . BS.toStrict . toLazyByteString <$> buildQuery "www.example.com" typeA
+  -- _checkDNSQuery sock -- Check sample query
   response <- recv sock 1024
   (C8.putStrLn . C8.fromStrict . B16.encode) response
-
-_checkDNSQuery :: Socket -> IO ()
-_checkDNSQuery =
-  flip sendAll $ B16.decodeLenient "82980100000100000000000003777777076578616d706c6503636f6d0000010001"
 
 data Header = Header
   { hId :: Word16,
@@ -56,7 +53,7 @@ typeA = 0x0001
 classIn :: Word16
 classIn = 0x0001
 
--- >>> B16.encode $ BS.toStrict $ toLazyByteString $  header2Bytes (Header 1 2 3 4 5 6)
+-- >>> _debugBuilderOutput $  header2Bytes (Header 1 2 3 4 5 6)
 -- "000100020003000400050006"
 header2Bytes :: Header -> Builder
 header2Bytes h =
@@ -83,19 +80,20 @@ encodeDNSName =
       buildWord w = (word8 . fromIntegral . BS.length) w <> lazyByteString w
    in C8.pack >>> C8.split '.' >>> fmap buildWord >>> mconcat >>> (<> word8 0)
 
--- >>> _debugBuilderOutput $ buildQuery "www.example.com" typeA
--- "82980100000100000000000003777777076578616d706c6503636f6d0000010001"
---
--- "82980100000100000000000003777777076578616d706c6503636f6d0000010001"
-buildQuery :: String -> Word16 -> Builder
-buildQuery domainName recordType =
+-- >>> _debugBuilderOutput <$> buildQuery "www.example.com" typeA
+-- "b83f0100000100000000000003777777076578616d706c6503636f6d0000010001"
+buildQuery :: (MonadIO m) => String -> Word16 -> m Builder
+buildQuery domainName recordType = do
+  qId <- fst . genWord16 <$> initStdGen -- Random number
   let name = encodeDNSName domainName
-      -- (qId, _) = genWord16 $ mkStdGen 1234 -- This obviously should be a true random number generator and not this.
-      qId = 0x8298
       recursionDesired = 0x0100 -- 1 << 8
       header = Header qId recursionDesired 1 0 0 0
       question = Question (toLazyByteString name) recordType classIn
-   in header2Bytes header <> question2Bytes question
+  pure $ header2Bytes header <> question2Bytes question
 
 _debugBuilderOutput :: Builder -> B.ByteString
 _debugBuilderOutput = B16.encode . BS.toStrict . toLazyByteString
+
+_checkDNSQuery :: Socket -> IO ()
+_checkDNSQuery =
+  flip sendAll $ B16.decodeLenient "82980100000100000000000003777777076578616d706c6503636f6d0000010001"
